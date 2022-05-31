894cc6cb0ce7b56b1c3988f1baa5100055b5bcd2
cv <nguyenhai20112003@gmail.com>
2022-06-01 00:21:25 +0700

    gh-pages

---
+++ asm1.js
@@ -0,0 +1,13 @@
+const menuIcon = document.querySelector("#menu-icon");
+const menu = document.querySelector("#menu");
+// Event
+let isMenuOpen = false;
+menuIcon.addEventListener("click", function (e) {
+  if (isMenuOpen == false) {
+    menu.classList.remove("hidden");
+    isMenuOpen = true;
+  } else {
+    menu.classList.add("hidden");
+    isMenuOpen = false;
+  }
+});
